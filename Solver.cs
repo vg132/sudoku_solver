bef9846da4b031ce51c6b798caaf0b87cbbb4f1a
Viktor Gars <viktor@vgsoftware.com>
2020-04-25 13:17:08 +0200

    added naked pair solution and visulizer

--- Solver.cs
+++ Solver.cs
@@ -6,24 +6,49 @@ namespace sudoku_solver
 {
 	public partial class Solver
 	{
-		private readonly SudokuField _field;
-
+		private int[] _field;
 		private IDictionary<int, IList<int>> _candidates;
 
-		public Solver(SudokuField field)
+		public void Solve(string field)
 		{
-			_field = field;
+			_field = new int[81];
+			for (int i = 0; i < field.Length; i++)
+			{
+				_field[i] = int.Parse(field.Substring(i, 1));
+			}
+
+			InitCandidates();
+			Console.WriteLine($"Unsolved: {_field.Where(item => item == 0).Count()}, Total candidates: {_candidates.Sum(item => item.Value.Count())}");
+			FindCandidates();
+			Console.WriteLine($"Is correct: {CheckIfCorrect()}");
+			Console.WriteLine($"Unsolved: {_field.Where(item => item == 0).Count()}, Total candidates: {_candidates.Sum(item => item.Value.Count())}");
+			DrawField();
 		}
 
-		public void Solve()
+		private bool CheckIfCorrect()
 		{
-			InitCandidates();
-			Console.WriteLine($"Candidates: {_candidates.Sum(item => item.Value.Count())}");
-			Console.WriteLine($"Candidates for pos 0: {string.Join(",", _candidates[0])}");
-			FindCandidates();
-			Console.WriteLine($"Candidates: {_candidates.Sum(item => item.Value.Count())}");
-			Console.WriteLine($"Candidates for pos 0: {string.Join(",", _candidates[0])}");
-			_field.DrawField();
+			for (int i = 0; i < 81; i++)
+			{
+				var pos = new Position(i);
+				if (!IsCorrect(pos.RowIndex))
+				{
+					return false;
+				}
+				if (!IsCorrect(pos.BoxIndex))
+				{
+					return false;
+				}
+				if (!IsCorrect(pos.ColumnIndex))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private bool IsCorrect(IEnumerable<int> indexList)
+		{
+			return _field.Where((item, index) => indexList.Contains(index)).Sum() == 45;
 		}
 
 		private void InitCandidates()
@@ -31,7 +56,7 @@ namespace sudoku_solver
 			_candidates = new Dictionary<int, IList<int>>();
 			for (int i = 0; i < 81; i++)
 			{
-				if (_field.Field[i] == 0)
+				if (_field[i] == 0)
 				{
 					_candidates.Add(i, Enumerable.Range(1, 9).ToList());
 				}
@@ -44,17 +69,85 @@ namespace sudoku_solver
 
 		private void FindCandidates()
 		{
-			for (int index = 0; index < 100; index++)
+			for (int index = 0; index < 200; index++)
 			{
 				for (int i = 0; i < 81; i++)
 				{
 					var pos = new Position(i);
-					if (_field.Field[i] == 0)
+					if (_field[i] == 0)
 					{
 						ClearCandidates(pos.Pos, pos.BoxIndex);
 						ClearCandidates(pos.Pos, pos.RowIndex);
 						ClearCandidates(pos.Pos, pos.ColumnIndex);
+
+						FindUniqueCandidates(pos.Pos, pos.BoxIndex);
+						FindUniqueCandidates(pos.Pos, pos.RowIndex);
+						FindUniqueCandidates(pos.Pos, pos.ColumnIndex);
+
+						FindNakedPair(pos.Pos, pos.BoxIndex);
+						FindNakedPair(pos.Pos, pos.RowIndex);
+						FindNakedPair(pos.Pos, pos.ColumnIndex);
 					}
+				}
+			}
+		}
+
+		private void FindNakedPair(int pos, IEnumerable<int> indexList)
+		{
+			if (_candidates[pos].Count != 2)
+			{
+				return;
+			}
+			int nakedPairIndex = -1;
+			foreach (var index in indexList)
+			{
+				if (index == pos)
+				{
+					break;
+				}
+				if (_candidates[index].Count != 2)
+				{
+					break;
+				}
+				if (_candidates[pos].Except(_candidates[index]).Any())
+				{
+					break;
+				}
+				nakedPairIndex = index;
+			}
+			if (nakedPairIndex > 0)
+			{
+				foreach (var index in indexList)
+				{
+					if (index != nakedPairIndex && index != pos)
+					{
+						foreach (var candidateToRemove in _candidates[pos])
+						{
+							_candidates[index].Remove(candidateToRemove);
+						}
+					}
+				}
+			}
+		}
+
+		private void FindUniqueCandidates(int pos, IEnumerable<int> indexList)
+		{
+			for (int i = 0; i < _candidates[pos].Count(); i++)
+			{
+				var candidate = _candidates[pos][i];
+				var unique = true;
+				foreach (var index in indexList)
+				{
+					if (index != pos && _candidates[index].Contains(candidate))
+					{
+						unique = false;
+						break;
+					}
+				}
+				if (unique)
+				{
+					_field[pos] = candidate;
+					_candidates[pos].Clear();
 				}
 			}
 		}
@@ -65,16 +158,37 @@ namespace sudoku_solver
 			{
 				if (index != pos)
 				{
-					if (_field.Field[index] != 0)
+					if (_field[index] != 0)
 					{
-						_candidates[pos].Remove(_field.Field[index]);
+						_candidates[pos].Remove(_field[index]);
 						if (_candidates[pos].Count == 1)
 						{
-							_field.Field[pos] = _candidates[pos].First();
+							_field[pos] = _candidates[pos].First();
 							_candidates[pos].Clear();
 						}
 					}
 				}
+			}
+		}
+
+		public void DrawField()
+		{
+			Console.WriteLine();
+			Console.WriteLine("===================");
+			Console.WriteLine("====== Field ======");
+			Console.WriteLine();
+			for (int line = 0; line < 9; line++)
+			{
+				Console.Write("\n|");
+				DrawLine(line);
+			}
+		}
+
+		private void DrawLine(int line)
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				Console.Write("{0}|", _field[(line * 9) + i]);
 			}
 		}
 	}
